3c070b1d5157c0dbda6ca23bf4b943f8ef06e09c
TomasCarzolio <tcarzolio@udesa.edu.ar>
2022-04-05 15:12:40 -0300

    Controladores y rutas

---
+++ proyectoKonfet/controllers/userController.js
@@ -0,0 +1,21 @@
+const data = require("../db/data");
+
+const indexController = {
+    header: function(req, res){
+    res.render()
+    }, 
+
+    footer: function(req, res){
+    res.render()
+    }, 
+
+    register: function(req, res){
+    res.render()
+    }, 
+
+    register: function(req, res){
+    res.render()
+    }, 
+}
+
+module.exports = (indexController);
--- proyectoKonfet/db/data.js
+++ proyectoKonfet/db/data.js
@@ -1,4 +1,4 @@
-const datos = {
+let datos = {
     usuario: {},
 
     productos:
