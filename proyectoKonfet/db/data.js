6c42cf5ecb959c298c1211d9dd41781a9a0c7644
chiarasalvu <csalvucci@udesa.edu.ar>
2022-04-06 19:02:45 -0300

    Avanzamos con database

--- proyectoKonfet/db/data.js
+++ proyectoKonfet/db/data.js
@@ -1,85 +1,128 @@
 let datos = {
-    usuario: {},
+    usuario: {
+        nombre: "Lisandro",
+        apellido: "Perez",
+        email: "lperez@gmail.com",
+        fechaDeNac: 02 / 02 / 1982,
+        numeroDeDoc: 44189323,
+        fotoDePerfil: "/images/retrato.jpeg",
+    },
 
     productos:
         [
             {
+                id: 1,
                 artista: "Daddy Yankee",
                 lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
+                fecha: 03 / 04 / 2022,
                 precio: "$2000",
             },
             {
-                artista: "Daddy Yankee",
+                id: 2,
+                artista: "Bad Bunny",
                 lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
+                fecha: 01 / 02 / 2022,
+                precio: "$3000",
+            },
+            {
+                id: 3,
+                artista: "Bruno Mars",
+                lugar: "Obras",
+                fecha: 05 / 09 / 2023,
                 precio: "$2000",
             },
             {
-                artista: "Daddy Yankee",
+                id: 4,
+                artista: "Nicky Jam",
                 lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
+                fecha: 08 / 10 / 2022,
+                precio: "$1000",
+            },
+            {
+                id: 5,
+                artista: "Maria Becerra",
+                lugar: "Estadio Luna Park",
+                fecha: 01 / 12 / 2024,
+                precio: "$500",
+            },
+            {
+                id: 6,
+                artista: "Duki",
+                lugar: "Estadio Luna Park",
+                fecha: 25 / 04 / 2022,
+                precio: "$2500",
+            },
+            {
+                id: 7,
+                artista: "Emilia Mernes",
+                lugar: "Estadio Luna Park",
+                fecha: 02 / 02 / 2023,
+                precio: "$3000",
+            },
+            {
+                id: 8,
+                artista: "Tiago PZK",
+                lugar: "Estadio Luna Park",
+                fecha: 15 / 06 / 2024,
+                precio: "$5000",
+            },
+            {
+                id: 9,
+                artista: "Lit Killah",
+                lugar: "Estadio Luna Park",
+                fecha: 22 / 09 / 2022,
                 precio: "$2000",
             },
             {
-                artista: "Daddy Yankee",
+                id: 10,
+                artista: "Nicki Nicole",
                 lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
-                precio: "$2000",
+                fecha: 12 / 08 / 2022,
+                precio: "$3000",
             },
-            {
-                artista: "Daddy Yankee",
-                lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
-                precio: "$2000",
-            },
-            {
-                artista: "Daddy Yankee",
-                lugar: "Estadio Luna Park",
-                fecha: "03/04/2022",
-                precio: "$2000",
-            }
         ],
-    comentarios: [{
-        nombreUsuario: 'Tomás Carzolio',
-        comentario: 'Me pareció un excelente concierto! Sin duda alguna compraré por esta pagina cuando venga nuevamente.'
-    },
-    {
-        nombreUsuario: 'Chiara Salvucci',
-        comentario: 'Me pareció un mal concierto concierto! El artista no fue lo que esperaba. Igualmente, la página ha sido muy eficaz a la hora de comprar. Muchas gracias!'
-    },
-    {
-        nombreUsuario: 'Nicolás Cucher',
-        comentario: 'Maravilloso concierto! Muchas gracias por la facilidad para obtener la entrada!'
-    },
-    {
-        nombreUsuario: 'Carolina Sierras',
-        comentario: 'Me pareció un buen concierto concierto!'
-    },
-    {
-        nombreUsuario: 'Lucas Demiral',
-        comentario: ' La página ha sido muy correcta a la hora de comprar. Agradecidisimo!'
-    },
-    {
-        nombreUsuario: ' Thomas Hanks',
-        comentario: 'Nice concert!'
-    },
-    {
-        nombreUsuario: ' Carlos Iglesias',
-        comentario: 'Muchas gracias!'
-    },
-    {
-        nombreUsuario: 'Lionel Scaloni',
-        comentario: 'Un espectaculo!'
-    },
-    {
-        nombreUsuario: 'Javier Ortega',
-        comentario: 'Pesimo concierto!'
-    },
-    {
-        nombreUsuario: 'Lucas Perez',
-        comentario: 'Que contento que estoy! Muchas gracias por la entrega!'
-    }
+
+    comentarios: [
+        {
+            nombreUsuario: 'Tomás Carzolio',
+            comentario: 'Me pareció un excelente concierto! Sin duda alguna compraré por esta pagina cuando venga nuevamente.'
+        },
+        {
+            nombreUsuario: 'Chiara Salvucci',
+            comentario: 'Me pareció un mal concierto concierto! El artista no fue lo que esperaba. Igualmente, la página ha sido muy eficaz a la hora de comprar. Muchas gracias!'
+        },
+        {
+            nombreUsuario: 'Nicolás Cucher',
+            comentario: 'Maravilloso concierto! Muchas gracias por la facilidad para obtener la entrada!'
+        },
+        {
+            nombreUsuario: 'Carolina Sierras',
+            comentario: 'Me pareció un buen concierto concierto!'
+        },
+        {
+            nombreUsuario: 'Lucas Demiral',
+            comentario: ' La página ha sido muy correcta a la hora de comprar. Agradecidisimo!'
+        },
+        {
+            nombreUsuario: ' Thomas Hanks',
+            comentario: 'Nice concert!'
+        },
+        {
+            nombreUsuario: ' Carlos Iglesias',
+            comentario: 'Muchas gracias!'
+        },
+        {
+            nombreUsuario: 'Lionel Scaloni',
+            comentario: 'Un espectaculo!'
+        },
+        {
+            nombreUsuario: 'Javier Ortega',
+            comentario: 'Pesimo concierto!'
+        },
+        {
+            nombreUsuario: 'Lucas Perez',
+            comentario: 'Que contento que estoy! Muchas gracias por la entrega!'
+        }
     ]
 };
 
