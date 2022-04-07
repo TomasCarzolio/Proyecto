56e0f004f3a8c0d7153a1d96396563e42283754b
NicolasCucher <ncucher@udesa.edu.ar>
2022-04-07 16:08:46 -0300

    Cambiando las vistas de ejs

--- proyectoKonfet/controllers/indexController.js
+++ proyectoKonfet/controllers/indexController.js
@@ -1,16 +1,19 @@
 const data = require('../db/data')
 
 const indexController = {
-    index : function (req, res) {
-        res.render('index')
+    index: function (req, res) {
+        res.render('index',
+            { productos: data.productos },
+            { comentario: data.comentarios },
+            { usuario: data.usuario })
     },
-    register : function (req, res) {
+    register: function (req, res) {
         res.render('register')
     },
-    login : function (req, res) {
+    login: function (req, res) {
         res.render('login')
     },
-    
+
 }
 
 module.exports = indexController;
