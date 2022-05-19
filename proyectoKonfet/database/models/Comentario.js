47e6458c950827030ce3f4ab1f8950a32f6b119b
TomasCarzolio <tcarzolio@udesa.edu.ar>
2022-05-19 15:51:36 -0300

    Creando modelos

---
+++ proyectoKonfet/database/models/Comentario.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, dataTypes) {
+    const cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        creado : { type: dataTypes.DATE },
+        contenido: { type: dataTypes.STRING },
+       usuario_id: { type: dataTypes.INTEGER },
+        producto_id: { type: dataTypes.INTEGER  }
+
+
+    }
+
+    const configs = {
+        tableName: 'comentarios',
+        timestamps: false
+    }
+    const Comentario = sequelize.define('Comentario', cols, configs);
+    return Comentario;
+}
