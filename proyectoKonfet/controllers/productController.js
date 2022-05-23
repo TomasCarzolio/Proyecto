e7e9af8e47e7c38c1e4b3b2e9ca92a8b774ff2c1
TomasCarzolio <tcarzolio@udesa.edu.ar>
2022-05-23 19:24:45 -0300

    Pasando controlador de usuario a modulo externo

--- proyectoKonfet/controllers/productController.js
+++ proyectoKonfet/controllers/productController.js
@@ -1,13 +1,19 @@
-const data = require('../db/data')
+const data = require('../db/data');
+const comentarios = data.comentarios;
+const productos = data.productos;
 
 const controller = {
     product: function (req, res) {
-        res.render('product', 
-        {
-            comentarios: data.comentarios,
-            productos: data.productos[req.params.product],
-        })
-    },
+        productos.findAll()
+        .then(function(productos){
+       comentarios.findAll()
+       .then(function(comentarios){
+      res.render('product', 
+        { comentarios: comentarios,
+          productos: productos[req.params.product]})
+                })
+             })
+        },
     productAdd: function (req, res) {
         res.render('product-add')
     }
@@ -15,3 +21,14 @@ const controller = {
 }
 
 module.exports = controller;
+
+// index: function (req, res) {
+//     producto.findAll()
+//         .then(function(productos){
+//             res.render("index", {productos : productos, })
+//         });
+//         comentarios.findAll()
+//         .then(function(comentarios){
+//             res.render("index", {comentarios : comentarios})
+//         }
+//         )},
