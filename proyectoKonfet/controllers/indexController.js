6bebd87ae7e83a103aae2314dbf0ca21445e487e
NicolasCucher <ncucher@udesa.edu.ar>
2022-05-30 15:50:35 -0300

    Agregando a app.js session y cookies, cambiando los formularios en indexController y descargando las librerias necesarias (session y bcrscrypts)

--- proyectoKonfet/controllers/indexController.js
+++ proyectoKonfet/controllers/indexController.js
@@ -1,20 +1,78 @@
 const data = require('../database/models')
 const producto = data.Producto;
-const comentarios = db.comentarios;
+const comentario = data.Comentario;
+const usuario = data.Usuario;
+const hasher = require('bcryptjs')
+
 
 const controller = {
     index: function (req, res) {
-        res.render('index',
-            {
-                productos: data.productos,
+        producto.findAll()
+            .then(function (productos) {
+                comentario.findAll()
+                    .then(function (comentarios) {
+                        res.render("index", {
+                            productos: productos,
+                            comentarios: comentarios
+                        })
+                    });
             })
+            .catch(function (error) {
+                res.send(error)
+            });
+
     },
     register: function (req, res) {
-        res.render('register')
+        res.render('register');
     },
+
+    store: function (req, res) {
+        if (!req.body.email) { throw Error('No se proporciona correo electrónico.') }
+        const hashedContrasenia = hasher.hashSync(req.body.contrasenia, 10);
+        usuario.create({
+            username: req.body.nombreUsuario,
+            contrasenia: hashedContrasenia,
+            email: req.body.email,
+        })
+            .then(function () {
+                res.redirect('/');
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
+
     login: function (req, res) {
         res.render('login')
     },
+
+    access: function (req, res) {
+        usuario.findOne({ where: { nombreUsuario: req.body.nombreUsuario } })
+            .then(function(usuario){
+                if (!usuario) throw Error('No se encuentra usuario.')
+                if (hasher.compareSync(req.body.contrasenia, usuario.contrasenia)) {
+                    req.session.usuario = usuario;
+                    if (req.body.rememberme) {
+                        // Guardo del lado del cliente 
+                        res.cookie('usuarioId', usuario.id, {maxAge : 1000 * 60 *60 * 7})
+                    }
+                    res.redirect('/')
+                }
+                else {
+                    throw Error('Credencial invalida')
+                }
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
+
+    logout: function (req, res) {
+        req.session.usuario = null;
+        res.clearCookie('usuarioId');
+        res.redirect('/')
+    },
+
     searchResults: function (req, res) {
         res.render('search-results')
     }
--- proyectoKonfet/database/models/Usuario.js
+++ proyectoKonfet/database/models/Usuario.js
@@ -10,7 +10,8 @@ module.exports = function (sequelize, dataTypes) {
         email: { type: dataTypes.STRING },
         fotoDePerfil: { type: dataTypes.STRING },
         fechaDeNac: { type: dataTypes.DATE },
-        dni: { type: dataTypes.INTEGER }
+        dni: { type: dataTypes.INTEGER },
+        contrasenia : { type : dataTypes.STRING}
     }
     const configs = {
         tableName: 'usuarios',
