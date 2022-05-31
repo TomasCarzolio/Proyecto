bb8075da23abaf5eb2102c0a5a9c6fde242471e2
NicolasCucher <ncucher@udesa.edu.ar>
2022-05-30 21:39:21 -0300

    Cambiando configuraciones en app.js

--- proyectoKonfet/app.js
+++ proyectoKonfet/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var data = require('../database/models')
+var data = require('./database/models')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
