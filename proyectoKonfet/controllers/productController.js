099fa2d2ce37d3c44a9d9dda78bd03c5a1a9843b
NicolasCucher <ncucher@udesa.edu.ar>
2022-06-13 17:05:02 -0300

    Cambiamos las terminaciones del controlador y las vistas de product-edit

--- proyectoKonfet/controllers/productController.js
+++ proyectoKonfet/controllers/productController.js
@@ -5,56 +5,57 @@ const comentario = data.Comentario;
 const controller = {
     product: function (req, res) {
         producto.findByPk(req.params.producto)
-        .then(function(producto){
-        comentario.findAll ({where: {producto_id: req.params.producto}})
-        .then(function(comentarios){
-            res.render('product', {producto, comentarios})
-        });
-   })
-   .catch(function(error){
-       res.send(error)
-   })
+            .then(function (producto) {
+                comentario.findAll({ where: { producto_id: req.params.producto } })
+                    .then(function (comentarios) {
+                        res.render('product', { producto, comentarios })
+                    });
+            })
+            .catch(function (error) {
+                res.send(error)
+            })
 
     },
 
     productAdd: function (req, res) {
-        if (!req.session.usuario) { 
+        if (!req.session.usuario) {
             res.redirect('/login');
-        } 
+        }
         res.render('product-add');
-},
+    },
 
-store: function(req, res) {
-    req.body.usuario_id = req.session.usuario.id;
-    if (req.file) req.body.foto = (req.file.path).replace('public', '');
-    data.productos.create(req.body)
-        .then(function() {
-            res.redirect('/')
-        })
-        .catch(function(error) {
-            res.send(error);
-        })
-},
+    store: function (req, res) {
+        req.body.usuario_id = req.session.usuario.id;
+        if (req.file) req.body.foto = (req.file.path).replace('public', '');
+        data.productos.create(req.body)
+            .then(function () {
+                res.redirect('/')
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
 
-    edit: function(req, res) {
-    producto.findByPk(req.params.id)
-        .then(function (productos) {
-            res.render('product-edit', { productos });
-        })
-        .catch(function (error) {
-            res.send(error);
-        })
-},
+    edit: function (req, res) {
+        producto.findByPk(req.params.id)
+            .then(function (producto) {
+                res.render('product-edit', { producto });
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    },
 
-    update: function(req, res) {
-    if (req.file) req.body.foto = (req.file.path).replace('public', '');
-    producto.update(req.body, { where: { id: req.params.id } })
-        .then(function() {
-            res.redirect('/')
-        })
-        .catch(function(error) {
-            res.send(error);
-        })
-}};
+    update: function (req, res) {
+        if (req.file) req.body.foto = (req.file.path).replace('public', '');
+        producto.update(req.body, { where: { id: req.params.id } })
+            .then(function () {
+                res.redirect('/')
+            })
+            .catch(function (error) {
+                res.send(error);
+            })
+    }
+};
 
 module.exports = controller;
